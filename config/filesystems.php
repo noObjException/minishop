8203a1775c24a1395d8b081ac43eb4d0250f5149
Jerry <767839489@qq.com>
2018-03-03 12:16:31 +0800

    change config && add language package

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,12 @@ return [
             'bucket' => env('AWS_BUCKET'),
         ],
 
+        'admin' => [
+            'driver' => 'local',
+            'root' => public_path('uploads'),
+            'visibility' => 'public',
+            'url' => env('APP_URL').'/uploads',
+        ],
     ],
 
 ];
---
+++ resources/lang/zh-CN/field.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'title'       => '标题',
+    'name'        => '名称',
+    'url'         => '链接',
+    'image'       => '图片',
+    'sort'        => '排序',
+    'status'      => '状态',
+    'parent'      => '上级',
+    'price'       => '价格',
+    'description' => '描述',
+    'detail'      => '详情',
+
+    'open'  => '启用',
+    'close' => '禁用',
+
+    'created_at' => '创建时间',
+    'updated_at' => '修改时间',
+    'deleted_at' => '删除时间',
+
+    'category' => '分类',
+];
