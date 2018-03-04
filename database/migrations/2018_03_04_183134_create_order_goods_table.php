497dcc0004d22f1f8934d6b4bba3604183b6819b
Jerry <767839489@qq.com>
2018-03-04 23:31:29 +0800

    上线

---
+++ database/migrations/2018_03_04_183134_create_order_goods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_goods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id')->index();
+            $table->unsignedInteger('goods_id')->index();
+            $table->decimal('total_price')->default('0.00');
+            $table->unsignedInteger('total');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_goods');
+    }
+}
